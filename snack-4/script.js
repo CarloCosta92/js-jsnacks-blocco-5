77efb53de20fdf4e7e7c1f5881b5520491feccc4
Carlo Costa <carlo.costa92@gmail.com>
2025-03-12 14:23:11 +0100

    feat: solved 4 ex

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const scrittori = posts.map((element) => element.author);
+console.log(scrittori);
